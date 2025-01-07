f4a3c88fe3c34ffb53b525365fe13ae60485fece
Idyege Nathaniel <idyegenathaniel@gmail.com>
2025-01-07 22:22:45 +0100

    Update

--- frontend/src/Components/Hero.jsx
+++ frontend/src/Components/Hero.jsx
@@ -1,39 +1,37 @@
-import heroImg from "../assets/assets_frontend/header_img.png";
-import groupHeroImg from "../assets/assets_frontend/group_profiles.png";
-import { FaArrowRight } from "react-icons/fa";
+import heroImg from "../assets/header_img.png";
+import groupHeroImg from "../assets/group_profiles.png";
+import { FaLongArrowAltRight } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
-    <section className="bg-primary text-white">
-      <div className="grid grid-cols-1 md:grid-cols-2 justify-center items-center px-6 md:px-10">
-        <div className="">
-          <h3 className="text-3xl md:text-5xl font-semibold leading-tight">
-            Book Appointment
-            <br />
-            With Trusted Doctors
-          </h3>
-          <span className="flex mt-5">
-            <img src={groupHeroImg} alt="group-profile" className="w-28" />
-            <p className="md:text-sm  gap-5">
-              Simply browse through our extensive list of trusted doctors,{" "}
-              <br />
-              schedule your appointment hassle-free.
-            </p>
-          </span>
-          <button className="flex px-8 py-3 bg-white text-black rounded-full font-light">
-            Book appointment
-            <FaArrowRight />
-          </button>
-        </div>
+    <section className="bg-primary rounded-lg text-white grid grid-cols-1 md:grid-cols-2 justify-center items-center px-6 md:px-10">
+      <div className=" flex flex-col items-start justify-center gap-4 m-auto">
+        <h3 className="text-3xl md:text-54xl font-semibold leading-tight">
+          Book Appointment
+          <br />
+          With Trusted Doctors
+        </h3>
+        <span className="flex mt-5">
+          <img src={groupHeroImg} alt="group-profile" className="w-28" />
+          <p className="md:text-sm mb-5  gap-5">
+            Simply browse through our extensive list of trusted doctors, <br />
+            schedule your appointment hassle-free.
+          </p>
+        </span>
+        <Link className="inline-block items-center gap-4 px-8 py-3 bg-white text-black rounded-full font-light">
+          Book appointment
+          <FaLongArrowAltRight className="font-light" />
+        </Link>
+      </div>
 
-        {/* RIGHT */}
-        <div className="">
-          <img
-            src={heroImg}
-            alt=""
-            className="w-full  h-auto bottom-0 rounded-lg"
-          />
-        </div>
+      {/* RIGHT */}
+      <div className="">
+        <img
+          src={heroImg}
+          alt=""
+          className="w-full  h-auto bottom-0 rounded-lg"
+        />
       </div>
     </section>
   );
--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,7 +1,16 @@
-import { NavLink } from "react-router-dom";
-import Logo from "../assets/assets_frontend/logo.svg";
+import { Navigate, NavLink, useNavigate } from "react-router-dom";
+import Logo from "../assets/logo.svg";
+import { useState } from "react";
+import { assets } from "../assets/assets";
+import { FaChevronDown } from "react-icons/fa";
 
 const Navbar = () => {
+  const [menu, setMenu] = useState(false);
+  const [token, setToken] = useState(true);
+
+  const navigate = useNavigate();
+
+  const pStyle = "hover:text-black";
   return (
     <nav className="flex justify-between items-center mb-5 py-4 border-b border-[#ADADAD]">
       <NavLink to={"/"}>
@@ -17,9 +26,35 @@ const Navbar = () => {
         ))}
       </ul>
       <div className="flex items-center gap-4">
-        <button className="hidden md:block px-8 py-3 bg-primary text-white rounded-full font-light">
-          Create Account
-        </button>
+        {token ? (
+          <div className="flex items-center gap-2 group relative cursor-pointer">
+            <img className="w-8 rounded-full" src={assets.profile_pic} alt="" />
+            <FaChevronDown />
+            <div className="top-0 right-0 absolute pt-14 text-base text-gray-600 z-20 hidden group-hover:block">
+              <div className="bg-stone-100 min-w-40 flex flex-col rounded gap-4 py-4 px-3">
+                <p onClick={() => navigate("my-profile")} className={pStyle}>
+                  My Profile
+                </p>
+                <p
+                  onClick={() => navigate("my-appointment")}
+                  className={pStyle}
+                >
+                  My Appointment
+                </p>
+                <p onClick={() => setToken(false)} className={pStyle}>
+                  Log Out
+                </p>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <button
+            onClick={() => navigate("/login")}
+            className="hidden md:block px-8 py-3 bg-primary text-white rounded-full font-light"
+          >
+            Create Account
+          </button>
+        )}
       </div>
     </nav>
   );
