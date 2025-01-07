2d332cb9511ade733ba288744f5df127a47f401a
Idyege Nathaniel <idyegenathaniel@gmail.com>
2025-01-07 22:46:39 +0100

    Update

--- frontend/src/Components/Hero.jsx
+++ frontend/src/Components/Hero.jsx
@@ -7,19 +7,19 @@ const Hero = () => {
   return (
     <section className="bg-primary rounded-lg text-white grid grid-cols-1 md:grid-cols-2 justify-center items-center px-6 md:px-10">
       <div className=" flex flex-col items-start justify-center gap-4 m-auto">
-        <h3 className="text-3xl md:text-54xl font-semibold leading-tight">
+        <h3 className="text-3xl md:text-5xl font-semibold leading-tight">
           Book Appointment
           <br />
           With Trusted Doctors
         </h3>
-        <span className="flex mt-5">
-          <img src={groupHeroImg} alt="group-profile" className="w-28" />
+        <span className="flex mt-5 gap-2">
+          <img src={groupHeroImg} alt="group-profile" className="w-" />
           <p className="md:text-sm mb-5  gap-5">
             Simply browse through our extensive list of trusted doctors, <br />
             schedule your appointment hassle-free.
           </p>
         </span>
-        <Link className="inline-block items-center gap-4 px-8 py-3 bg-white text-black rounded-full font-light">
+        <Link className="flex justify-center items-center gap-4 px-8 py-3 bg-white text-black rounded-full font-light">
           Book appointment
           <FaLongArrowAltRight className="font-light" />
         </Link>
