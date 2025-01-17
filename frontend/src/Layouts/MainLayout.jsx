5906ba9001cf7e393868aea1b97a90f18fd82cb4
Idyege Nathaniel <idyegenathaniel@gmail.com>
2025-01-17 13:23:57 +0100

    Layout exception

--- frontend/src/Layouts/MainLayout.jsx
+++ frontend/src/Layouts/MainLayout.jsx
@@ -1,13 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
 
 const MainLayout = () => {
+  const location = useLocation();
+  const noFooterPath = ["/login"];
+
+  const shouldShowFooter = !noFooterPath.includes(location.pathname);
   return (
     <>
       <Navbar />
       <Outlet />
-      <Footer />
+      {shouldShowFooter && <Footer />}
     </>
   );
 };
