fbef4353b5addcdea29e6ae40a33c40621b9ce52
Idyege Nathaniel <idyegenathaniel@gmail.com>
2025-02-13 20:16:53 +0100

    Update

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,4 +1,7 @@
-
+import validator from "validator";
+import bcrypt from "bcrypt"
+import { v2 as cloudinary } from "cloudinary"
+import doctorModel from "../models/doctorModel";
 
 
 
@@ -9,10 +12,53 @@ const addDoctor = async (req, res) => {
         const { name, email, password, speciality, degree, experience, about, fees, address } = req.body
         const imageFile = req.file
 
-        console.log({ name, email, password, speciality, degree, experience, about, fees, address }, imageFile)
+       // console.log({ name, email, password, speciality, degree, experience, about, fees, address }, imageFile)
+
+       //SEARCHING FOR ALL DATA TO ADD TO DOCTORS
+
+       if(!name || !email || !password || !speciality || degree|| !experience || !about || !fees || !address){
+            return res.json({success: false, message: "missing details"})
+       }
+
+       //VALIDATING EMAIL
+       if(!validator.isEmail(email )){
+            return res.json({success: false, message: "Enter a valid Email"})
+       }
+       //VALIDATING STRONG PASSWORD
+       if(password.length < 8){
+          return res.json({success: false, message: "Enter a strong password"})
+       }
+
+       //HASHING DOCTOR PASSWORD
+       const salt = await bcrypt.genSalt(10)
+       const hashedPassword = await bcrypt.hash(password, salt)
+
+       //IMAGE UPLOAD 
+       const imageUpload = await cloudinary.uploader.upload(imageFile.path, {resource_type: "image"})
+       const imageUrl = imageUpload.secure_url
+
+       const doctorData = {
+          name,
+          email,
+          image: imageUrl,
+          password: hashedPassword,
+          speciality,
+          degree,
+          experience,
+          about,
+          fees,
+          address: JSON.parse(address),
+          date: Date.now()
+       }
+
+       const newDoctor = new doctorModel(doctorData)
+       newDoctor.save()
+
+       res.json({success: true, message: "Doctor successfully added"})
 
     } catch (error) {
-        
+     console.log(error)
+        res.json({success: false, message: error.message})
     }
 }
 
